2a93bc5975735045420f08cb7ac2664ea3a825f2
Laurent <laurent.veyrat2@gmail.com>
2026-08-07 17:04:30 +0200

    Update validation for the input socialSecurityNumber + add checkbox isFirstResponder

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function ProfileScreen() {
   return (
@@ -11,8 +11,8 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 24,
