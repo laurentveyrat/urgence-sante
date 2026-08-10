a096841fa2d27ed60d96ee00bedc3c9786e3c150
Matthieu Saudi <saudimatthieu@gmail.com>
2026-08-10 11:03:12 +0200

    finishin redux

--- screens/CreateAccountScreen.js
+++ screens/CreateAccountScreen.js
@@ -7,7 +7,9 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Ionicons } from "@expo/vector-icons";
+import { login } from "../reducers/user";
 
 // --------------------- VALIDATION ------------------
 function validateNumeroSecu(value) {
@@ -54,6 +56,8 @@ function validatePassword(value) {
 }
 
 export default function CreateAccountScreen({ navigation }) {
+  const dispatch = useDispatch();
+
   // --------------------- STATES ------------------
   const [numeroSecu, setNumeroSecu] = useState("");
   const [email, setEmail] = useState("");
@@ -122,6 +126,7 @@ export default function CreateAccountScreen({ navigation }) {
       const data = await response.json();
 
       if (data.result) {
+        dispatch(login({ email: data.email, token: data.token, socialSecurityNumber: data.socialSecurityNumber, }));
         navigation.navigate("MainTabs");
       } else {
         setSubmitError(data.error || "Une erreur est survenue");
